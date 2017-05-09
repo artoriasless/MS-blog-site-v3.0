a53ce5bd05d7446b90b90831b87f1603c8e897ba
Stan <tangjianyi1993@live.com>
2017-05-09 11:00:37 +0800

    fix some bugs for linux

--- log.js
+++ log.js
@@ -2,14 +2,26 @@
 var MAX_VISIT_TIME = 6;
 /* 用于记录客户端访问的ip地址，设置黑名单 */
 var fs = require('fs');
+/* 设置时间格式（在linux下的 tolocalString 和window下的格式不同） */
+var formatTime = (date) => {
+    var timeMonth  = (String((parseInt(date.getMonth()) + 1)).length == 1) ? ('0' + (parseInt(date.getMonth()) + 1)) : (parseInt(date.getMonth()) + 1),
+        timeDay    = (String(parseInt(date.getDate())).length == 1) ? ('0' + (parseInt(date.getDate()))) : (parseInt(date.getDate())),
+        timeLeft   = date.getFullYear() + '-' + timeMonth + '-' + timeDay,
+        
+        timeHour   = (String(date.getHours()).length == 1) ? ('0' + date.getHours()) : date.getHours(),
+        timeMinute = (String(date.getMinutes()).length == 1) ? ('0' + date.getMinutes()) : date.getMinutes(),
+        timeSecond = (String(date.getSeconds()).length == 1) ? ('0' + date.getSeconds()) : date.getSeconds(),
+        timeRight  = timeHour + ':' + timeMinute + ':' + timeSecond;
+    
+    /* yyyy-mm-dd hh:mm:ss */
+    return (timeLeft + ' ' + timeRight);
+};
 
 var log = function(ip, callbackFunc) {
-    const currentDate = new Date();
-    const fullDateStr = currentDate.toLocaleString();
-    const dateStr     = fullDateStr.slice(0, 16).replace(/\-|\s|\:/g, '_');
-    const fileName    = './logs/' + dateStr + '.json';
-
-    var blackListTag = false;
+    var fullDateStr  = formatTime(new Date()),
+        dateStr      = fullDateStr.slice(0, 16).replace(/\-|\s|\:/g, '_'),
+        fileName     = './logs/' + dateStr + '.json';
+        blackListTag = false;
 
     fs.readFile(fileName, function(err, data) {
         if (err) {
